8396969e80034d250a42f5a37e566f16f15bb327
am-devops09 <am.devops09@gmail.com>
2023-09-22 21:11:58 +0100

    fixing bugs

--- modules/s3_bucket/s3_bucket.tf
+++ modules/s3_bucket/s3_bucket.tf
@@ -9,4 +9,12 @@ resource "aws_s3_bucket" "s3_demo" {
 resource "aws_s3_bucket_acl" "s3-acl" {
   bucket = aws_s3_bucket.s3_demo.id
   acl = "private"
+  depends_on = [ aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership ]
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.s3_demo.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
 }
